116966cdf41cae363ff5fb08b587d25262bb4f9d
Sethu Madhavan <kpsanal@live.in>
2018-07-29 21:06:38 +0530

    initial

---
+++ new-instance-setup.sh
@@ -0,0 +1,15 @@
+#############################################################################
+#
+#	This script is to setup all basic requirements of instances 
+#	running at T&F AWS Cloud
+#
+#############################################################################
+
+# Set Banner
+sudo wget http://tools.ap.corplan.net/banner.txt -O /etc/banner.txt
+sudo sed -i '/Banner/c\Banner /etc/banner.txt' /etc/ssh/sshd_config
+sudo service ssh restart || sudo service sshd restart;
+
+# Setup NTP
+
+# IP Tables
