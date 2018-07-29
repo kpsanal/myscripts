9452ec59b50e3a018508f6a4f61407ab04f8c322
Sethu Madhavan <kpsanal@live.in>
2018-07-29 23:38:30 +0530

    version 1.4 with site specific update

--- setup_checkmk_agent.sh
+++ setup_checkmk_agent.sh
@@ -1,13 +1,38 @@
 #################################################################
 #
 #			CheckMK Agent Setup
-#	Date: July 29, 2018		Version: 1.0
+#
+#	Author: SANAL K P		Team: AP Global Infra
+#	Date: July 29, 2018		Version: 1.4
 #
 #################################################################
 #/bin/bash!
-filehost=YOUR-WEB-Server
-checkmk=Your-CheckMK-Server
 package="xinetd"
+FileServer="files.yourdomain.com"
+cmkServer="checkmk.yourdomain.com"
+secret="YOUR-Secret"
+
+hostname=`hostname`
+loc=`echo $hostname | cut -c 1-4`
+	if [ "$loc" == "use1" ] || [ "$loc" == "USE1" ]; then
+		{
+		folder="tandfus"
+		site="SITE1"
+		}
+	elif [ "$loc" == "euw1" ] || [ "$loc" == "EUW1" ]; then
+		{
+		folder="tandfuk"
+		site="SITE2"
+		}
+	else	
+		{
+		folder="";
+		site="SITE3"
+		}
+	fi
+ip=`ifconfig | awk '{print $2}' | grep -w 10 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
+curl "http://$cmkServer/TANDF/check_mk/webapi.py?action=add_host&_username=automation&_secret=$secret" -d 'request={"hostname":"'$hostname'","folder":"'$folder'","attributes":{"ipaddress":"'$ip'","site":"'$site'","tag_agent":"cmk-agent"}}'
+
 if [ -n "$(command -v lsb_release)" ]; then
 	distroname=$(lsb_release -s -d)
 elif [ -f "/etc/os-release" ]; then
@@ -34,8 +59,8 @@ if [ "$OS" == "Ubuntu" ]; then
 		iptables -I INPUT -p tcp -s 10.0.0.0/8 --dport 6556 -j ACCEPT
 		apt-get install xinetd -y
 		apt-get install wget -y
-		wget -q -t 1 --timeout=10 http://$filehost/uploads/cmk.deb -O /tmp/cmk.deb; sudo dpkg -i /tmp/cmk.deb;
-		wget http://$filehost/uploads/check_mk -O /etc/xinetd.d/check_mk
+		wget -q -t 1 --timeout=10 http://$FileServer/uploads/cmk.deb -O /tmp/cmk.deb; sudo dpkg -i /tmp/cmk.deb;
+		wget http://$FileServer/uploads/check_mk -O /etc/xinetd.d/check_mk
 		rm -f /tmp/cmk.deb
 		if [ "$REL" -le "14" ]; then
 			{
@@ -45,7 +70,7 @@ if [ "$OS" == "Ubuntu" ]; then
 		elif [ "$REL" -gt "14" ]; then
                      {
                         #Ubuntu 14 or latest
-			sudo systemctl restart $package;sudo systemctl enable $package;sudo snap restart $package;
+			sudo systemctl restart $package;sudo systemctl enable $package;
                         }
 		fi
 		}
@@ -61,13 +86,13 @@ elif [ "$OS" == "Amazon" ]; then
 		yum makecache fast
 		yum install xinetd -y
 		sudo yum install wget -y
-		sudo yum install -y http://$filehost/uploads/check-mk-agent-1.4.0p18-1.noarch.rpm;
-		wget http://$filehost/uploads/check_mk -O /etc/xinetd.d/check_mk
+		sudo yum install -y http://$FileServer/uploads/check-mk-agent-1.4.0p18-1.noarch.rpm;
+		wget http://$FileServer/uploads/check_mk -O /etc/xinetd.d/check_mk
 		
 		if [ "$AREL" -ge "2018" ]; then
 			{
 			#Amazone Linux 2018
-			sudo systemctl restart $package;sudo systemctl enable $package;
+			sudo service $package restart;
 			}
 		elif [ "$AREL" -le "2017" ]; then
                         {
@@ -90,8 +115,8 @@ elif [ "$OS" == "CentOS" ]; then
 		{	
 			iptables -I INPUT -p tcp -s 10.0.0.0/8 --dport 6556 -j ACCEPT
 			sudo install wget -y
-			sudo yum install -y http://$filehost/uploads/check-mk-agent-1.4.0p18-1.noarch.rpm;
-			wget http://$filehost/uploads/check_mk -O /etc/xinetd.d/check_mk
+			sudo yum install -y http://$FileServer/uploads/check-mk-agent-1.4.0p18-1.noarch.rpm;
+			wget http://$FileServer/uploads/check_mk -O /etc/xinetd.d/check_mk
 			sudo systemctl enable $package;sudo systemctl restart $package;sudo restart $package;
 		}
 	else 
@@ -99,7 +124,7 @@ elif [ "$OS" == "CentOS" ]; then
 			echo "Package $package is already installed";
 		}
 	fi
-
+	}
 else
 	{
 		echo "I'm sorry.. I had a hard time understanding the OS Platform" >&2;
@@ -107,15 +132,4 @@ else
 	}
 fi
 
-hostname=`hostname`
-loc=`echo $hostname | cut -c 1-4`
-	if [ $loc == use1 ], then
-		folder="TandFUS"
-	elif [ $loc == euw1 ], then
-		folder="TandFUK"
-	else
-		folder="";
-	fi
-ip=`ifconfig | awk '{print $2}' | grep -w 10 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
-curl "http://$checmkserver/TANDF/check_mk/webapi.py?action=add_host&_username=automation&_secret=YOUR-SECRET" -d 'request={"hostname":"$hostname","folder":"$folder","attributes":{"ipaddress":"$ip","site":"TANDF","tag_agent":"cmk-agent"}}'
 # END
