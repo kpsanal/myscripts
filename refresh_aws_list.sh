a7aa2e0d979f5592ad7cfe927416d2c9202e7a0a
Sethu Madhavan <kpsanal@live.in>
2019-08-20 21:10:24 +0530

    clean code

--- refresh_aws_list.sh
+++ refresh_aws_list.sh
@@ -1,7 +1,7 @@
 ######################################################################
 #
 #               AWS Instance List
-#       Updated: July 30, 2018    Version: 3.2
+#       Updated: Aug 20, 2019    Version: 3.4
 #
 ######################################################################
 #!/bin/bash
@@ -9,14 +9,10 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export SHELL=/bin/bash
 
-for u in ap-user appe-user;
-        do
         for r in eu-west-1 us-east-1;
                 do
-                ac=`echo $u | cut -d'-' -f1`
-                aws ec2 describe-instances --region $r --profile $u --output text --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, InstanceId, InstanceType, KeyName, State.Name, LaunchTime, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`Environment`].Value] [0][0], [Tags[?Key==`Author`].Value] [0][0], [Tags[?Key==`Service`].Value] [0][0] ]' > /opt/opstools/AWSInstances/instances-$ac-$r.csv
-                sed -i 's/\t/;/g' /opt/opstools/AWSInstances/instances-$ac-$r.csv
-                echo -e "\nCompleted $ac $r\n";
+                aws ec2 describe-instances --region $r --output text --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, InstanceId, InstanceType, KeyName, State.Name, LaunchTime, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`Environment`].Value] [0][0], [Tags[?Key==`Author`].Value] [0][0], [Tags[?Key==`Service`].Value] [0][0] ]' > /opt/opstools/AWSInstances/instances-$r.csv
+                sed -i 's/\t/;/g' /opt/opstools/AWSInstances/instances-$r.csv
+                echo -e "\nCompleted $r\n";
                 done;
-        done;
 #END
