e2645776a3b6a64829d28454373803a6361f45ba
Sethu Madhavan <kpsanal@live.in>
2021-06-23 07:57:45 +0530

    Updating KV integration

--- connect.sh
+++ connect.sh
@@ -1,66 +1,79 @@
+#!/bin/bash
 ######################################################################
 #
-#   Updated: June 11, 2018    Version: 3.2
+#   Updated: June 23, 2021    Version: 4.3
 #
-#	Script will help to reduce manual entry of IP or KeyName
-#	of AWS instances running on all accounts
+#       Script will help to reduce manual entry of IP or KeyName
+#       of AWS instances running on all accounts
 #
 #######################################################################
-#/bin/bash!
-KeysDir="/opt/opstools/KEYS"
 HostsDir="/opt/opstools/AWSInstances"
-pass="XYZ-PASS";
+CONSUL="127.0.0.1"
+ENV="PROD-SSH"
 ue=$1
 if [ -z $ue ]; then
-	{
-	clear;
+        {
+        clear;
         echo -e "\n\t\t\t***** AWS Connect Tool *****\n";
-	echo -e "\n\n\tUasge:\n\t\t connect <HOST NAME> or <IP Address> or <Instance Id>\n\n";
-	exit 0;
-	}
+        echo -e "\n\n\tUasge:\n\t\t connect <HOST NAME> or <IP Address> or <Instance Id>\n\n";
+        exit 0;
+        }
 fi
+
+get_inventory(){
+
 inventory=`grep -iRw $ue $HostsDir/*`;
 sugg=`grep -iR $ue $HostsDir/* | cut -d';' --output-delimiter=$'\t' -f11,8 | sort`;
 ac=`echo $inventory | awk '{print $1}' | cut -d'-' -f2`;
 AC=`echo $ac | tr '[:lower:]' '[:upper:]'`;
 if [ -z "$inventory" ]; then
-	{
-	clear;
-	echo -e "\n\t\t\t***** AWS Connect Tool *****\n";
-	echo -e "\n***** Error!! Invalid Host $ue";
+        {
+        clear;
+        echo -e "\n\t\t\t***** AWS Connect Tool *****\n";
+        echo -e "\n***** Error!! Invalid Host $ue";
         echo -e "\n\n\tUasge:\n\t\t connect <HOST NAME> or <IP Address> or <Instance Id>\n\n";
-	echo -e "May be, you trying to connect..?? huh?? \n\n$sugg\n";
+        echo -e "May be, you trying to connect..?? huh?? \n\n$sugg\n";
         exit 0;
-	}
+        }
 else
-	{
+        {
                 ip=`echo $inventory | cut -d';' -f8`;
                 hn=`echo $inventory | cut -d';' -f11`;
                 kf=`echo $inventory | cut -d';' -f4`;
                 au=`echo $inventory | cut -d';' -f13`;
                 env=`echo $inventory | cut -d';' -f12`;
                 gp=`echo $inventory | cut -d';' -f14`;
-                key="$KeysDir/$ac-ec2-keys/$kf.pem";
-		sudo chown $USER:netops $key
-		sudo chmod 600 $key
-	}
+
+        }
 fi
-clear
+}
+
+get_key(){
+        key="/tmp/$kf";
+        curl -s http://$CONSUL/v1/kv/$ENV/$kf  | jq -r '.[].Value | @base64d' > $key
+        sudo chown $USER:users $key
+        sudo chmod 600 $key
+
+}
+
+get_inventory
+get_key
+
 echo -e "\n\t\t\t***** AWS Connect Tool *****\n";
-	echo -e "\n IP: $ip \t\t\t\tAuthor: $au\n Hostname: $hn \t\t\tEnvironment: $AC $env\n KeyName: $kf \t\t\t\tGroup: $gp\n";
+        echo -e "\n IP: $ip \t\t\t\tAuthor: $au\n Hostname: $hn \t\t\t\tEnvironment: $AC $env\n KeyName: $kf \t\t\t\tGroup: $gp\n";
 if  [ -e $key ]; then
-	{
-#echo "KEY -- $key";
-	ssh -i $key -oBatchMode=yes -oStrictHostKeyChecking=no -oConnectTimeout=10 ubuntu@$ip || ssh -i $key -oBatchMode=yes -oStrictHostKeyChecking=no -oConnectTimeout=10 ec2-user@$ip || ssh -i $key -oBatchMode=yes -oStrictHostKeyChecking=no -oConnectTimeout=10 centos@$ip || sshpass -p $pass ssh -oStrictHostKeyChecking=no -oConnectTimeout=10 sysadmin@$ip || echo -e "\nI'm sorry...... :( $ue is NOT Accessible\n" < /dev/null;
-	echo -e "\n\tGood Bye!!\n"
-	exit 0;
-	}
+        {
+        ssh -i $key -q -oBatchMode=yes -oStrictHostKeyChecking=no -oConnectTimeout=60 ubuntu@$ip || echo -e "\nI'm sorry...... :( $ue is NOT Accessible\n" < /dev/null;
+        echo -e "\n\tGood Bye!!\n"
+#       rm $key
+        exit 0;
+        }
 else
-	{
-        echo -e "\nThe Key $kf.pem not available. Please drop an email to Infra Team";
-	sshpass -p $pass ssh -oStrictHostKeyChecking=no -oConnectTimeout=10 sysadmin@$ip || echo -e "\nI'm sorry...... :( $ue is NOT Accessible\n\n" < /dev/null;
-	echo -e "\n\tGood Bye!!\n";
-	exit 0;
+        {
+        echo -e "\nThe Key $kf not available. Please drop an email to Infra Team";
+        echo -e "\nI'm sorry...... :( $ue is NOT Accessible\n\n" < /dev/null;
+        echo -e "\n\tGood Bye!!\n";
+        exit 1;
         }
 fi
 #END
